7ea36567b32ebabd60f98a3fa6035ca4c0b3c634
“Martin <mkuemmel@usm.lmu.de>
2020-01-27 14:32:13 +0100

    The vignet seems to be working.

--- SEImplementation/SEImplementation/Plugin/Vignet/VignetSourceTask.h
+++ SEImplementation/SEImplementation/Plugin/Vignet/VignetSourceTask.h
@@ -40,8 +40,11 @@
 #define _SEIMPLEMENTATION_PLUGIN_VIGNETSOURCETASK_H_
 
 #include "SEFramework/Task/SourceTask.h"
+#include "SEFramework/Property/DetectionFrame.h"
+#include "SEImplementation/Plugin/PixelCentroid/PixelCentroid.h"
 #include "SEImplementation/Plugin/Vignet/Vignet.h"
 #include "SEImplementation/Plugin/ShapeParameters/ShapeParameters.h"
+#include "SEImplementation/Measurement/MultithreadedMeasurement.h"
 
 namespace SourceXtractor {
 class VignetSourceTask : public SourceTask {
@@ -53,12 +56,46 @@ public:
   virtual ~VignetSourceTask() = default;
 
   virtual void computeProperties(SourceInterface& source) const {
-    // get the relevant properties
-    const auto& a_image = source.getProperty<ShapeParameters>().getEllipseA();
-    const auto& b_image = source.getProperty<ShapeParameters>().getEllipseB();
+    std::lock_guard<std::recursive_mutex> lock(MultithreadedMeasurement::g_global_mutex);
 
-    // compute and set the property
-    std::vector<SeFloat> vignet_vector(m_vignet_size[0]*m_vignet_size[0], (SeFloat)a_image/b_image);
+    const int& x_pix = (int)source.getProperty<PixelCentroid>().getCentroidX()+0.5;
+    const int& y_pix = (int)source.getProperty<PixelCentroid>().getCentroidY()+0.5;
+
+    // get the detection and the variance frames
+    const auto& sub_image     = source.getProperty<DetectionFrame>().getFrame()->getSubtractedImage();
+    const auto& var_image     = source.getProperty<DetectionFrame>().getFrame()->getVarianceMap();
+    const auto& var_threshold = source.getProperty<DetectionFrame>().getFrame()->getVarianceThreshold();
+
+    // get the sub-image boundaries
+    int x_start = x_pix-m_vignet_size[0]/2;
+    int y_start = y_pix-m_vignet_size[1]/2;
+    int x_end   = x_start+ m_vignet_size[0];
+    int y_end   = y_start+ m_vignet_size[1];
+
+    // create and fill the vignet vector
+    std::vector<SeFloat> vignet_vector(m_vignet_size[0]*m_vignet_size[1], m_vignet_default_pixval);
+    int index=0;
+    for (int ix=x_start; ix<x_end; ix++){
+      for (int iy=y_start; iy<y_end; iy++, index++){
+        //if (ix>-1 && iy>-1 && ix<sub_image->getWidth() && iy<sub_image->getHeight()) {
+        //  if (var_image->getValue(ix, iy)<var_threshold)
+        //    vignet_vector[index] =  sub_image->getValue(ix, iy);
+        //}
+        if (ix<0 || iy<0 || ix>=sub_image->getWidth() || iy>=sub_image->getHeight()) {
+          //vignet_vector[index] = m_vignet_default_pixval;
+          continue;
+        }
+        //else {
+         if (var_image->getValue(ix, iy)<var_threshold)
+           vignet_vector[index] =  sub_image->getValue(ix, iy);
+         // else
+            //vignet_vector[index] = m_vignet_default_pixval;
+         //   continue;
+        //}
+      }
+    }
+
+    // set the property
     source.setProperty<Vignet>(vignet_vector);
 };
 private:
